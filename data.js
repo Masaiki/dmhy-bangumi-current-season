3864bc7de0fb80504ce2c50fef72662a71967a06
菜姬 <mydarer@gmail.com>
2020-05-11 09:21:40 +0800

    v1

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-﻿var data = {
+﻿let data = {
 	"data": [
 		[],
 		[
--- dmhy-bangumi-current-season.js
+++ dmhy-bangumi-current-season.js
@@ -1,27 +1,50 @@
-let bangumi_data = data['data'];
-let week_name = ['週日（日）','週一（月）','週二（火）','週三（水）','週四（木）','週五（金）','週六（土）'];
-let trs = [];
-for (let i = 0; i < week_name.length; i++)
-{
-    let td = $('<td></td>');
-    for(let j = 0; j < bangumi_data[i].length; j++)
-    {
-        let name = bangumi_data[i][j][0];
-        let keyword = bangumi_data[i][j][1]||name;
-        td.append(`<a href="/topics/list?keyword=${encodeURIComponent(keyword)}">${name}</a>`)
+function loadJS( url, callback ){
+    var script = document.createElement('script'),
+        fn = callback || function(){};
+    script.type = 'text/javascript';
+    //IE
+    if(script.readyState){
+        script.onreadystatechange = function(){
+            if( script.readyState == 'loaded' || script.readyState == 'complete' ){
+                script.onreadystatechange = null;
+                fn();
+            }
+        };
+    }else{
+        //其他浏览器
+        script.onload = function(){
+            fn();
+        };
     }
-    let tr = $(`<tr><th>${week_name[i]}</th></tr>`).append(td);
-    trs.push(tr);
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
-$(".jmd").ready(function(){
-    let d = new Date();
-    let day = d.getDay();
-    $(".jmd")
-        .empty()
-        .append(trs[(d.getDay() + 5) % 7])
-        .append(trs[(d.getDay() + 6) % 7])
-        .append(trs[d.getDay()].addClass('today'))
-        .append(trs[(d.getDay() + 1) % 7]);
-    $(".jmd tr:even").addClass("even");
-    $(".jmd tr:odd").addClass("odd");
+loadJS('https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season/data.js', function(){
+    let bangumi_data = data['data'];
+    let week_name = ['週日（日）','週一（月）','週二（火）','週三（水）','週四（木）','週五（金）','週六（土）'];
+    let trs = [];
+    for (let i = 0; i < week_name.length; i++)
+    {
+        let td = $('<td></td>');
+        for(let j = 0; j < bangumi_data[i].length; j++)
+        {
+            let name = bangumi_data[i][j][0];
+            let keyword = bangumi_data[i][j][1]||name;
+            td.append(`<a href="/topics/list?keyword=${encodeURIComponent(keyword)}">${name}</a>`)
+        }
+        let tr = $(`<tr><th>${week_name[i]}</th></tr>`).append(td);
+        trs.push(tr);
+    }
+    $(".jmd").ready(function(){
+        let d = new Date();
+        let day = d.getDay();
+        $(".jmd")
+            .empty()
+            .append(trs[(d.getDay() + 5) % 7])
+            .append(trs[(d.getDay() + 6) % 7])
+            .append(trs[d.getDay()].addClass('today'))
+            .append(trs[(d.getDay() + 1) % 7]);
+        $(".jmd tr:even").addClass("even");
+        $(".jmd tr:odd").addClass("odd");
+    });
 });
