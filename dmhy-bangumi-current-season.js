d4f25ae653ebaac10c0703c8a07ad02592d91d7b
VegeHime <mydarer@gmail.com>
2020-07-13 19:25:30 +0800

    update to 0.4.0

--- dmhy-bangumi-current-season.js
+++ dmhy-bangumi-current-season.js
@@ -2,7 +2,7 @@
 // @name         dmhy-bangumi-current-season
 // @name:zh-CN   动漫花园新番索引更新脚本
 // @namespace    https://github.com/VegeHime/dmhy-bangumi-current-season
-// @version      0.3.0
+// @version      0.4.0
 // @description  update data of new season bangumi on dmhy
 // @description:zh-CN 更新动漫花园新番列表及搜索关键词
 // @author       Vegehime
@@ -21,14 +21,11 @@
         while (this.firstChild) this.removeChild(this.firstChild);
         return this;
     }
-    function createElementWithAttr(TagName, Attrs, Text) {
+    function createElementWithAttr(TagName, Attrs = {}, Text = '') {
         let element = document.createElement(TagName);
-        if (Attrs) {
-            for (let attr in Attrs)
-                element[attr] = Attrs[attr];
-        }
-        if (Text)
-            element.textContent = Text;
+        for (let attr in Attrs)
+            element[attr] = Attrs[attr];
+        element.textContent = Text;
         return element;
     }
     function loadJS(url, callback) {
@@ -39,33 +36,20 @@
             fn();
         };
         script.src = url;
-        document.querySelector('head').appendChild(script);
+        document.head.appendChild(script);
     }
     let ShowAllBangumi = localStorage.ShowAllBangumi !== "false";
-    let day = new Date().getDay();
+    let today = new Date();
+    let day = today.getDay();
     let trs = [];
     let panel = document.querySelector(".jmd");
-    function bangumiRefresh() {
-        panel.empty();
-        if (ShowAllBangumi)
-            trs.forEach((item) => { panel.appendChild(item); });
-        else {
-            for (let i = 5; i <= 8; ++i)
-                panel.appendChild(trs[(day + i) % 7]);
-            panel.appendChild(trs[7]);
-        }
-        panel.childNodes.forEach((item, index) => {
-            if (index % 2) item.classList.add("odd");
-            else item.classList.add("even");
-        });
-    }
-    document.querySelector("div[id$='_ad']").removeAttribute('align');
-    loadJS('https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/data.js', function () {
-        let bangumi_data = data['data'];
-        let week_name = ['週日（日）', '週一（月）', '週二（火）', '週三（水）', '週四（木）', '週五（金）', '週六（土）', '非週更'];
+    function data2doms() {
+        trs = [];
         for (let i = 0; i < bangumi_data.length; i++) {
             let td = document.createElement('td');
             for (let j = 0; j < bangumi_data[i].length; j++) {
+                if (bangumi_data[i][j][2] && today < new Date(bangumi_data[i][j][2])) continue;
+                if (bangumi_data[i][j][3] && today > new Date(bangumi_data[i][j][3])) continue;
                 let name = bangumi_data[i][j][0];
                 let keyword = bangumi_data[i][j][1] || name;
                 let link = createElementWithAttr('a', {
@@ -74,23 +58,58 @@
                 td.appendChild(link);
             }
             let tr = document.createElement('tr');
-            let th = createElementWithAttr('th', null, week_name[i]);
+            let th = createElementWithAttr('th', null, bangumi_group_name[i]);
             tr.appendChild(th);
             tr.appendChild(td);
             trs.push(tr);
         }
         trs[day].classList.add('today');
-        let switchButton = createElementWithAttr('a', {
-            'href': "javascript:;",
-            'style': "margin-right:2px;"
-        }, '顯示切換');
-        switchButton.onclick = () => {
-            ShowAllBangumi = !ShowAllBangumi;
-            localStorage.ShowAllBangumi = ShowAllBangumi;
-            bangumiRefresh();
-        };
-        let block = document.querySelector('span.fr');
-        block.insertBefore(switchButton, block.firstChild);
+    }
+    function bangumiRefresh() {
+        panel.empty();
+        if (ShowAllBangumi)
+            trs.forEach((item) => { panel.appendChild(item); });
+        else {
+            for (let i = 5; i <= 8; ++i)
+                panel.appendChild(trs[(day + i) % 7]);
+            for (let i = 7; i < bangumi_data.length; ++i)
+                panel.appendChild(trs[i]);
+        }
+        panel.childNodes.forEach((item, index) => {
+            if (index % 2) item.classList.add("odd");
+            else item.classList.add("even");
+        });
+    }
+    document.querySelector("div[id$='_ad']").removeAttribute('align');
+    let DataURL = localStorage.DataURL || 'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js';
+    loadJS(DataURL, function () {
+        data2doms();
         bangumiRefresh();
+    });
+    let switchButton = createElementWithAttr('a', {
+        'href': "javascript:;",
+        'style': "margin-right:2px;"
+    }, '顯示切換');
+    switchButton.onclick = () => {
+        ShowAllBangumi = !ShowAllBangumi;
+        localStorage.ShowAllBangumi = ShowAllBangumi;
+        bangumiRefresh();
+    };
+    let select = createElementWithAttr('select', {'style':'marginRight:2px; background-color:#247; color:#FFF'});
+    select.onchange = (e)=>{
+        localStorage.DataURL = (history_list.urls)[select.selectedIndex];
+        location.reload();
+    };
+    document.querySelector('.nav_title').style.paddingBottom = '5px';
+    let block = document.querySelector('span.fr');
+    block.insertBefore(switchButton, block.firstChild);
+    block.insertBefore(select, block.firstChild);
+    loadJS('https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/history-list.js', function () {
+        for (let i=0; i < history_list.values.length; i++) {
+            let option = createElementWithAttr('option',{'value':(history_list.values)[i]}, (history_list.names)[i]);
+            select.add(option);
+        }
+        let selectedValue = history_list.urls.indexOf(DataURL);
+        if (selectedValue !== -1) select.value = selectedValue;
     });
 })();
---
+++ history-list.js
@@ -0,0 +1,8 @@
+let history_list = {
+    'values': ['default', '202004'],
+    'names': ['2020年七月(当季)', '2020年四月'],
+    'urls': [
+        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202004.js'
+    ]
+};
