af7b2163078565edf50aa83e8dc49d633712e05d
VegeHime <mydarer@gmail.com>
2020-10-12 20:23:18 +0800

    update history-list

--- history-list.js
+++ history-list.js
@@ -1,8 +1,9 @@
 let history_list = {
-    'values': ['default', '202004'],
-    'names': ['2020年七月(当季)', '2020年四月'],
+    'values': ['default', '202007', '202004'],
+    'names': ['2020年10月(当季)', '2020年7月', '2020年4月'],
     'urls': [
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202007.js',
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202004.js'
     ]
 };
