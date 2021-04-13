ea1ce2147cf3e30c4efa1171cb76f735016eeb11
MIRIMIRIM <vxzms@ya.ru>
2021-04-13 16:47:52 +0800

    update 202104

--- history-list.js
+++ history-list.js
@@ -1,8 +1,9 @@
 let history_list = {
     'values': ['default', '202010', '202007', '202004'],
-    'names': ['2021年01月(当季)', '2020年10月', '2020年7月', '2020年4月'],
+    'names': ['2021年04月(当季)','2021年01月', '2020年10月', '2020年7月', '2020年4月'],
     'urls': [
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202101.js',
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202010.js',
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202007.js',
         'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202004.js'
