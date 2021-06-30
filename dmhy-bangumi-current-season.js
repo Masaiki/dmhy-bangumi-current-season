f7b5415a4721526d241eb47fc206d4bea7a3ed30
Masaiki <mydarer@gmail.com>
2021-06-30 14:28:11 +0800

    update links

--- dmhy-bangumi-current-season.js
+++ dmhy-bangumi-current-season.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         dmhy-bangumi-current-season
 // @name:zh-CN   动漫花园新番索引更新脚本
-// @namespace    https://github.com/VegeHime/dmhy-bangumi-current-season
+// @namespace    https://github.com/Masaiki/dmhy-bangumi-current-season
 // @version      0.4.0
 // @description  update data of new season bangumi on dmhy
 // @description:zh-CN 更新动漫花园新番列表及搜索关键词
-// @author       Vegehime
+// @author       Masaiki
 // @match        *://dmhy.org/*
 // @match        *://www.dmhy.org/*
 // @match        *://share.dmhy.org/*
@@ -81,7 +81,7 @@
         });
     }
     document.querySelector("div[id$='_ad']").removeAttribute('align');
-    let DataURL = localStorage.DataURL || 'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js';
+    let DataURL = localStorage.DataURL || 'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js';
     loadJS(DataURL, function () {
         data2doms();
         bangumiRefresh();
@@ -104,7 +104,7 @@
     let block = document.querySelector('span.fr');
     block.insertBefore(switchButton, block.firstChild);
     block.insertBefore(select, block.firstChild);
-    loadJS('https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/history-list.js', function () {
+    loadJS('https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-list.js', function () {
         for (let i = 0; i < history_list.values.length; i++) {
             let option = createElementWithAttr('option', { 'value': history_list.values[i] }, history_list.names[i]);
             select.add(option);
--- history-list.js
+++ history-list.js
@@ -2,11 +2,11 @@ let history_list = {
     'values': ['default', '202104', '202101', '202010', '202007', '202004'],
     'names': ['2021年07月(当季)', '2021年04月','2021年01月', '2020年10月', '2020年7月', '2020年4月'],
     'urls': [
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/bangumi-data.js',
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202104.js',
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202101.js',
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202010.js',
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202007.js',
-        'https://cdn.jsdelivr.net/gh/VegeHime/dmhy-bangumi-current-season@master/202004.js'
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202104.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202101.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202010.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202007.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202004.js'
     ]
 };
