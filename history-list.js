507a65f4f76f7b3a4f91edc0150719a192652166
Masaiki <mydarer@gmail.com>
2021-06-30 21:28:47 +0800

    move history data into inner folder

--- history-list.js
+++ history-list.js
@@ -3,10 +3,10 @@ let history_list = {
     'names': ['2021年07月(当季)', '2021年04月','2021年01月', '2020年10月', '2020年7月', '2020年4月'],
     'urls': [
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js',
-        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202104.js',
-        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202101.js',
-        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202010.js',
-        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202007.js',
-        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/202004.js'
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202104.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202101.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202010.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202007.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202004.js'
     ]
 };
