81480ed621af8c2f56c4b9e7740a17f49f7fc77d
Masaiki <mydarer@gmail.com>
2021-07-26 09:02:48 +0800

    fix typo

--- history-list.js
+++ history-list.js
@@ -1,6 +1,6 @@
 let history_list = {
     'values': ['default', '202104', '202101', '202010', '202007', '202004'],
-    'names': ['2021年07月(当季)', '2021年04月','2021年01月', '2020年10月', '2020年7月', '2020年4月'],
+    'names': ['2021年07月(当季)', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
     'urls': [
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js',
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202104.js',
