386977d06b380f800499cd8c2da17ba9f3f2a9d9
MIRIMIRIM <vxzms@ya.ru>
2022-03-28 19:42:31 +0800

    update 2204

--- history-list.js
+++ history-list.js
@@ -1,8 +1,9 @@
 let history_list = {
-    'values': ['default', '202104', '202101', '202010', '202007', '202004'],
-    'names': ['2022年01月(当季)', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
+    'values': ['default', '202201', '202110', '202107', '202104', '202101', '202010', '202007', '202004'],
+    'names': ['2022年04月(当季)', '2022年01月', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
     'urls': [
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202201.js',
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202110.js',
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202107.js',
         'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202104.js',
