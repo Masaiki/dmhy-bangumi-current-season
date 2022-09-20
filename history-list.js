cae3cc414b72556f862b9a68824abeee6f52a99e
Theergold <1362474903@qq.com>
2022-09-20 17:30:11 +0800

    fix raw.githubusercontent.com

--- history-list.js
+++ history-list.js
@@ -2,15 +2,15 @@ let history_list = {
     'values': ['default', '202201', '202110', '202107', '202104', '202101', '202010', '202007', '202004'],
     'names': ['2022年07月(当季)', '2022年04月', '2022年01月', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
     'urls': [
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/bangumi-data.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202204.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202201.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202110.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202107.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202104.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202101.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202010.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202007.js',
-        'https://github.com/Theergold/dmhy-bangumi-current-season/raw/master/history-data/202004.js'
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/bangumi-data.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202204.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202201.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202110.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202107.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202104.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202101.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202010.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202007.js',
+        'https://raw.githubusercontent.com/Theergold/dmhy-bangumi-current-season/master/history-data/202004.js'
     ]
 };
