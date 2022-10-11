05d3789e005bb177738f9ccff45b1390ea97a248
Masaiki <mydarer@gmail.com>
2022-10-11 22:45:24 +0800

    revert repo changes

--- history-list.js
+++ history-list.js
@@ -2,15 +2,15 @@ let history_list = {
     'values': ['default', '202201', '202110', '202107', '202104', '202101', '202010', '202007', '202004'],
     'names': ['2022年07月(当季)', '2022年04月', '2022年01月', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
     'urls': [
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/bangumi-data.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202204.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202201.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202110.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202107.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202104.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202101.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202010.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202007.js',
-        'https://cdn.jsdelivr.net/gh/Theergold/dmhy-bangumi-current-season@master/history-data/202004.js'
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/bangumi-data.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202204.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202201.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202110.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202107.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202104.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202101.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202010.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202007.js',
+        'https://cdn.jsdelivr.net/gh/Masaiki/dmhy-bangumi-current-season@master/history-data/202004.js'
     ]
 };
