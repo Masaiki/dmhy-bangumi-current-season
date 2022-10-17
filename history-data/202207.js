48d8973843ffc72d6bb1105b93621d54b783ce4f
Theergold <1362474903@qq.com>
2022-10-17 19:56:14 +0800

    add 202010 (#5)

---
+++ history-data/202207.js
@@ -0,0 +1,90 @@
+let bangumi_group_name = [
+    '週日（日）',
+    '週一（月）',
+    '週二（火）',
+    '週三（水）',
+    '週四（木）',
+    '週五（金）',
+    '週六（土）',
+    '非週更'
+];
+let bangumi_data = [
+    [
+        ["LoveLive! Superstar!! 第2季", "LoveLive Superstar"],
+        ["異世界藥局"],
+        ["幽零幻鏡", "Yurei Deco"],
+        ["聯盟空軍航空魔法音樂隊 光輝魔女", "光輝魔女"],
+        ["RWBY 冰雪帝國", "RWBY"],
+        ["花火醬總是遲到"],
+        ["森林裡的熊先生，冬眠中。", "熊先生 冬眠"],
+        ["Tropical-Rouge！光之美少女", "Tropical-Rouge|Q娃"],
+        ["數碼寶貝 幽灵游戏", "數碼寶貝|數碼獸大冒險|數碼暴龍|Digimon Ghost Game"],
+        ["美妙魔法！", "美妙|PriMagi"],
+        ["寵物小精靈", "寶可夢|寵物小精靈"],
+        ["BORUTO-火影新世代-NARUTO NEXT GENERATIONS-", "火影|博人传"],
+        ["游戏王 GORUSH!!", "游戏王"]
+    ],
+    [
+        ["歡迎來到實力至上主義的教室 2nd Season", "欢迎来到实力至上主义的教室"],
+        ["轉生賢者的異世界生活 ～取得第二職業，成為世界最強～", "轉生賢者的異世界生活"],
+        ["風都偵探"],
+        ["卡片戰鬥!! 先導者 will+Dress", "卡片戰鬥 先導者"]
+    ],
+    [
+        ["OVERLORD Ⅳ", "OVERLORD"],
+        ["SHINE POST", "SHINE POST"],
+        ["金裝的維爾梅", "Kinsou no Vermeil"],
+        ["邪神與廚二病少女 X", " Dropkick"],
+        ["東京貓貓 New～♥", "Tokyo Mew Mew"]
+    ],
+    [
+        ["異世界舅舅", "Isekai Ojisan"],
+        ["異世界迷宮裡的後宮生活"],
+        ["繼母的拖油瓶是我的前女友"],
+        ["來自深淵 烈日的黃金鄉"],
+        ["綻放的阿爾斯諾特利亞 吸！", "Warau Arsnotoria Sun!"],
+        ["新網球王子U-17 WORLD CUP", "新網球王子"],
+        ["與貓共度的夜晚", "Yoru wa Neko to Issho"]
+    ],
+    [
+        ["徹夜之歌"],
+        ["即使如此依舊步步進逼", "Soredemo Ayumu wa Yosetekuru"],
+        ["打工吧！！魔王大人", "打工 魔王"],
+        ["組長女兒與照料專員", "組長女兒"],
+        ["奇米萌", "Chimimo"],
+        ["夏日重现", "夏日|summer time"]
+    ],
+    [
+        ["在地下城尋求邂逅是否搞錯了什麼IV 新章 迷宮篇", "Dungeon ni Deai"],
+        ["出租女友 第2季", "出租女友"],
+        ["天籟人偶", "Prima Doll"],
+        ["影宅 第2季", "Shadows House"],
+        ["神渣☆偶像", "神渣 偶像"],
+        ["惑星公主蜥蜴騎士"],
+        ["幕末替身傳說"]
+    ],
+    [
+        ["最近僱的女僕有點怪", "Saikin Yatotta Maid ga Ayashii"],
+        ["Lycoris Recoil"],
+        ["Engage Kiss"],
+        ["黑之召喚士"],
+        ["傳頌之物 二人的白皇", "傳頌之物"],
+        ["Extreme Hearts"],
+        ["足球風雲！ Goal to the Future", "足球風雲"],
+        ["頂點!!!!!!!!!!!!!!!", "頂點"],
+        ["武藏野！"],
+        ["冰冰冰 冰淇淋君2", "冰冰冰"],
+        ["杜鵑的婚約", "杜鵑 婚約"],
+        ["Love All Play"],
+        ["王者天下 第4季", "王者天下"]
+    ],
+    [
+        ["BanG Dream! Morfonication", "BanG Dream"],
+        ["Dr.STONE 龍水", "新石纪 龙水"],
+        ["賽博朋克 邊緣行者", "Cyberpunk"],
+        ["狂賭之淵：雙", "狂賭之淵"],
+        ["SPRIGGAN 保衛者", "轟天高校生"],
+        ["BASTARD!! －暗黑之破壞神－", "BASTARD"],
+        ["名侦探柯南", "柯南"]
+    ]
+];
