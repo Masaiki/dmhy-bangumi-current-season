cae6d401edb99d46cdbe56dff8721dee2af68ead
MIRIMIRIM <59959583+MIRIMIRIM@users.noreply.github.com>
2023-01-01 14:20:34 +0800

    add 2023.01

--- history-list.js
+++ history-list.js
@@ -1,8 +1,9 @@
 let history_list = {
-    'values': ['default', '202207', '202204', '202201', '202110', '202107', '202104', '202101', '202010', '202007', '202004'],
-    'names': ['2022年10月(当季)', '2022年07月', '2022年04月', '2022年01月', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
+    'values': ['default', '202210', '202207', '202204', '202201', '202110', '202107', '202104', '202101', '202010', '202007', '202004'],
+    'names': ['2023年01月(当季)', '2022年10月', '2022年07月', '2022年04月', '2022年01月', '2021年10月', '2021年07月', '2021年04月','2021年01月', '2020年10月', '2020年07月', '2020年04月'],
     'urls': [
         'https://aliyun-wb-bai621pis7-klt02wd7.oss-cn-shanghai.aliyuncs.com/dmhy-bangumi-current-season-master/bangumi-data.js',
+        'https://aliyun-wb-bai621pis7-klt02wd7.oss-cn-shanghai.aliyuncs.com/dmhy-bangumi-current-season-master/history-data/202210.js',
         'https://aliyun-wb-bai621pis7-klt02wd7.oss-cn-shanghai.aliyuncs.com/dmhy-bangumi-current-season-master/history-data/202207.js',
         'https://aliyun-wb-bai621pis7-klt02wd7.oss-cn-shanghai.aliyuncs.com/dmhy-bangumi-current-season-master/history-data/202204.js',
         'https://aliyun-wb-bai621pis7-klt02wd7.oss-cn-shanghai.aliyuncs.com/dmhy-bangumi-current-season-master/history-data/202201.js',
